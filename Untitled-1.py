01c7aac62acca8a6d46435c87386d4d184574bd5
Nathan-Zheng1 <nathanz32@nycstudents.net>
2025-11-10 07:16:12 -0800

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,3 +1,4 @@
+import random
 class Pet:
     def __init__(self, name, age, happiness, sleepiness, hunger, food, money):
         self.name = name
@@ -9,17 +10,14 @@ class Pet:
         self.money = money
     
     def work(self):
-        if self.sleepiness >= 5:
-            print("Kitty is too tired to work!")
-        else:
             self.money += 100
             self.sleepiness += 2
             self.hunger += 1
             self.happiness -= 1
-            print("Kitty is working! Increased cash. Increased Sleepiness and Hunger.")
+            print("Kitty is working! Increased cash. Increased Sleepiness and Hunger. Decreased happiness.")
 
     def play(self):
-        if self.sleepiness >= 5:
+        if self.sleepiness >= 8:
             print("Kitty is too tired to play!")
         else:
             self.happiness += 1
@@ -31,10 +29,14 @@ class Pet:
         if self.sleepiness <= 1:
             print("No need to sleep! Kitty is energetic!")
         else:
-            self.sleepiness += 1
-            self.hunger += 2
-            self.age += 1
-            print("Kitty is sleeping...but Kitty has become hungry and grown up! Sleep increased by 1. Hunger increased by 2. Age increased by 1.")
+            user_input = input("Are you sure you would like to proceed? Kitty will lose 2 sleepiness and age up at the cost of gaining 3 hunger.")
+            if user_input == "yes":
+                self.sleepiness -= 2
+                self.hunger += 3
+                self.age += 1
+                print("Bold move.")
+            elif user_input == "no":
+                print("Wise choice, come back later.")
         
     def shop(self):
         user_input = input("Welcome to cat central! Would you like to buy some cat food?")
@@ -55,11 +57,42 @@ class Pet:
                 print("Kitty does not have any food to eat.")
             else:
                 self.hunger -= 0.5
-                print("Kitty is feeling less hungry now! Hunger decreased by 1.")
+                self.food -= 0.5
+                print("Kitty is feeling less hungry now! Hunger decreased by 0.5")
+    
+    def wheel(self):
+        if self.money < 25:
+            print("Halt! You do not have enough money to play!")
+        else:
+            while True:
+                user_input = input("Would you like to spin the magnificent wheel of doom? The price per spin is 25 dollars! Spinning a value of 1 will win the huge jackpot! The values 2, 3, 4, or 5 will all award a prize...and the number 67... Type 'exit' to leave.")
+                if user_input == "exit":
+                    print("Goodbye!")
+                elif user_input == "yes":
+                    self.money -= 25
+                    x = random.randint(1,100)
+                    print(x)
+                    if x == 1:
+                        print("You won the jackpot! Cash increased by 1000. Happiness increased by 5")
+                        self.happiness += 5
+                        self.money += 1000
+                    elif x == 2 or x == 3 or x == 4 or x == 5:
+                        print("You won a prize! Increased cash by 50, increased cat food by 1. Happiness increased by 1.")
+                        self.money += 50
+                        self.food += 1
+                    elif x == 67:
+                        print("How unfortunate...cash decreased by 67...")
+                        self.money -= 67
+                    else:
+                        print("Unlucky! No winner this time. ")
+                elif user_input == "no":
+                    print("Come again next time!")
+                else:
+                    print("Invalid response.")
 
 Kitty = Pet("Kitty", 1, 5, 0, 5, 1, 100)
 
-print("Welcome to Kitty's adventure! In this game, you control a 'Pet' called Kitty! Kitty is able to run  a multitude of functions: Eat, Sleep, Play, Stats, Work, and Shop. Your goal is to make it to the age of 100! Keep him alive, eat him, kill him, or let him live a normal life! All the powers in your hands!")
+print("Welcome to Kitty's adventure! In this game, you control a 'Pet' called Kitty! Kitty is able to run  a multitude of functions: Eat, Sleep, Play, Stats, Work, and Shop. Your goal is to make it to the age of 100! To do this, you must be able to sleep 100 times, but be warned, each time you sleep is costly! Make sure to keep your stats good before you sleep, or kitty might just die...Keep him alive, eat him, kill him, or let him live a normal life! All the powers in your hands!")
 while True:
     user_input = input("Would you like to commit an action? Type 'exit' to leave.")
     if user_input == "exit":
@@ -86,3 +119,7 @@ while True:
         Kitty.work()
     elif user_input == "Shop":
         Kitty.shop()
+    elif user_input == "Gamble":
+        Kitty.wheel()
+    else:
+        print("Invalid input, try again.")
